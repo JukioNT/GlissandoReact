782dbf254ab8fa9350a33ae92418d91604254630
JukioNT <jjulio2468@gmail.com>
2022-09-02 07:28:06 -0300

    main update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import * as S from "./styles";
 import Sax from "img/Pintura (1).png"
+import Sax2 from "img/Pintura (2).png"
+import Guitar from "img/Pintura (3).png"
+import Violin from "img/Pintura (4).png"
+import Violin2 from "img/Pintura (5).png"
 
 const Home = () => {
     return(
@@ -13,6 +17,38 @@ const Home = () => {
                     </li>
                     <li>
                         <img src={ Sax } alt="" />
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <h2>Notas</h2>
+                    </li>
+                    <li>
+                        <img src={ Sax2 } alt="" />
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <h2>Acordes</h2>
+                    </li>
+                    <li>
+                        <img src={ Guitar } alt="" />
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <h2>Tempo</h2>
+                    </li>
+                    <li>
+                        <img src={ Violin } alt="" />
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <h2>Intervalos</h2>
+                    </li>
+                    <li>
+                        <img src={ Violin2 } alt="" />
                     </li>
                 </ul>
             </div>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -3,7 +3,36 @@ import { colors } from "styles/GlobalStyle"
 
 export const Home = styled.section`
     background-color: ${colors.primary};
+    height: 100%;
     color: whitesmoke;
     display: flex;
     justify-content: center;
+    flex-direction: column;
+    h1{
+        margin-top: 5px;
+        display: flex;
+        justify-content: center;
+        height: 40px;
+        width: 100%;
+    }
+    div{
+        margin-top: 20px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 90%;
+        ul{
+            li{
+                margin: 5px;
+                list-style-type: none;
+                h2{
+                    margin-left: 15Ppx;
+                    justify-content: center;
+                }
+                img{
+                    height: 250px;
+                }
+            }
+        }
+    }
 `
