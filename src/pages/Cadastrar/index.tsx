01839e0f8750b50fcc96036a9e2b8645947c94cc
JukioNT <jjulio2468@gmail.com>
2022-11-04 07:43:17 -0300

    started backent integration

--- src/pages/Cadastrar/index.tsx
+++ src/pages/Cadastrar/index.tsx
@@ -1,9 +1,40 @@
 import React from "react";
 import Person02 from "../../img/Person02.gif";
+import { FormEvent, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 import * as S from "../Login/styles";
+import { IErrorResponse, IUser } from "interfaces/user.interface";
+import { AxiosError } from "axios";
+import { apiUser } from "services/data";
 
 const Cadastrar = () => {
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState<IUser>({
+      name: '',
+      email: '',
+      password: '',
+    })
+    async function handleChange(e: IUser) {
+      setFormData((state: IUser | undefined) => ({ ...state, ...e }))
+    }
+    async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+      event.preventDefault()
+      try {
+        await apiUser.register(formData);
+        toast.success("Cadastro realizado com sucesso!");
+        navigate('/login')
+      } catch (error) {
+        const err = error as AxiosError<IErrorResponse>
+        let messages = err.response?.data.message
+        if (err.response?.data.errors) {
+          messages = err.response?.data.errors?.map((i) => i.message)
+            .reduce((total, cur) => `${total} ${cur}`)
+        }
+        toast.error(messages)
+      }
+    }
     return (
         <S.Section>
             <div className="main">
@@ -16,15 +47,21 @@ const Cadastrar = () => {
                         <h1>Cadastro</h1>
                         <div className="textfield">
                             <label htmlFor="usuario">Usuario</label>
-                            <input type="text" name="usuario" placeholder="Usuário" />
+                            <input type="text" name="usuario" placeholder="Usuário" 
+                            onChange={(e) => handleChange({ name: e.target.value })}
+                            value = {formData?.name}/>
+                        </div>
+                        <div className="textfield">
+                            <label htmlFor="senha">Email</label>
+                            <input type="text" name="email" placeholder="Email" 
+                            onChange={(e) => handleChange({ email: e.target.value })}
+                            value = {formData?.email}/>
                         </div>
                         <div className="textfield">
                             <label htmlFor="senha">Senha</label>
-                            <input type="password" name="senha" placeholder="Senha" />
-                        </div>
-                        <div className="textfield">
-                            <label htmlFor="senha">Repita sua senha</label>
-                            <input type="password" name="senha" placeholder="Senha" />
+                            <input type="password" name="senha" placeholder="Senha" 
+                            onChange={(e) => handleChange({ password: e.target.value })}
+                            value = {formData?.password}/>
                         </div>
                         <button className="button">Cadastrar</button>
                     </div>
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,41 @@
+import { FormEvent, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import React from "react";
 import Lottie from "lottie-react";
 import Person01 from "../../img/Person01.gif"
+import { toast } from "react-toastify";
 
 import * as S from "../Login/styles";
+import { useAuth } from "hooks/authHook";
+import { IErrorResponse, IUser } from "interfaces/user.interface";
+import { AxiosError } from "axios";
 
 const Login = () =>{
+    const navigate = useNavigate();
+    const { signIn } = useAuth()
+    const [formData, setFormData] = useState<IUser>({
+      email: '',
+      password: '',
+    })
+    async function handleChange(e: IUser) {
+      setFormData((state: IUser | undefined) => ({ ...state, ...e }))
+    }
+    async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+      event.preventDefault()
+      try {
+        const { email, password } = formData
+        await signIn({
+          email: String(email),
+          password: String(password),
+        })
+        toast.success("Login realizado com sucesso!");
+        navigate('/adm')
+      } catch (error) {
+        const err = error as AxiosError<IErrorResponse>
+        toast.error(String(err.response?.data))
+      }
+    }
+  
     return(
         <S.Section>
             <div className="main">
@@ -16,14 +47,22 @@ const Login = () =>{
                     <div className="form">
                         <h1>LOGIN</h1>
                         <div className="textfield">
-                            <label htmlFor="usuario">Usuario</label>
-                            <input type="text" name="usuario" placeholder="Usuário" />
+                            <label htmlFor="usuario">Email</label>
+                            <input type="email" name="email" id="email" placeholder="Email" 
+                            onChange={(e) => handleChange({ email: e.target.value })}
+                            value={formData?.email}
+                        />
                         </div>
                         <div className="textfield">
                             <label htmlFor="senha">Senha</label>
-                            <input type="password" name="senha" placeholder="Senha" />
+                            <input type="password" name="senha" id="senha" placeholder="Senha" 
+                             onChange={(e) => handleChange({ password: e.target.value })}
+                             value={formData?.password}
+                        />
                         </div>
-                        <button className="button">Login</button>
+                        <p>
+                            <button className="button">Login</button>
+                        </p>
                     </div>
                 </div>
             </div>
